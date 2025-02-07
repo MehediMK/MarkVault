53587e04b73713ffe20d9fa16498829ebb45e3e4
MehediMK <mehedikhan.cse@gmail.com>
2025-02-07 21:55:49 +0600

    Removed file stroge system

--- blog/utils.py
+++ blog/utils.py
@@ -2,17 +2,31 @@ import os
 import json
 
 
-def save_to_file(title, content, format_choice):
-    directory = "blogs"
-    os.makedirs(directory, exist_ok=True)  # Ensure directory exists
+# def save_to_file(title, content, format_choice):
+#     directory = "blogs"
+#     os.makedirs(directory, exist_ok=True)  # Ensure directory exists
 
+#     if format_choice == "md":
+#         file_path = os.path.join(directory, f"{title.replace(' ', '_')}.md")
+#         with open(file_path, "w") as file:
+#             file.write(f"# {title}\n\n{content}")
+#     elif format_choice == "json":
+#         file_path = os.path.join(directory, f"{title.replace(' ', '_')}.json")
+#         with open(file_path, "w") as file:
+#             json.dump({"title": title, "content": content}, file)
+
+#     return file_path
+
+
+def prepare_content_to_save(title, content, format_choice):
+    file_name = "draft.md"
+    
     if format_choice == "md":
-        file_path = os.path.join(directory, f"{title.replace(' ', '_')}.md")
-        with open(file_path, "w") as file:
-            file.write(f"# {title}\n\n{content}")
+        file_name = f"{title.replace(' ', '_')}.md"        
+        content = f"# {title}\n\n{content}"
+            
     elif format_choice == "json":
-        file_path = os.path.join(directory, f"{title.replace(' ', '_')}.json")
-        with open(file_path, "w") as file:
-            json.dump({"title": title, "content": content}, file)
+        file_name = f"{title.replace(' ', '_')}.json"
+        content = json.dumps({"title": title, "content": content}, indent = 4) 
 
-    return file_path
+    return file_name, content
